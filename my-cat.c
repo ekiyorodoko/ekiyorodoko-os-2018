f22810e1c02d278115f205809b669ee3247dd74e
Ekiyor Odoko <ekiyorodoko@gmail.com>
2018-09-16 00:08:36 +0100

    Pseudocode for my implementation of linux's cat

--- my-cat.c
+++ my-cat.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    // Loop to read file
+
+        // Check if file exists
+        // Get lines and print
+
+    //return file
+        
+    
+}
+
+//code segment to read file
 
 
