0a1ee06a8bc953fb4e5c1a9ea296f1ed8b86e2cd
Ekiyor Odoko <ekiyorodoko@gmail.com>
2018-09-18 16:28:43 +0100

    Finalized implementation of grep

--- my-grep.c
+++ my-grep.c
@@ -1,26 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // My implementation of linux's grep function
 int main(int argc, char *argv[]){
-    if (argc > 1){
-        // Check the parameters provided
-        if(argc==2){ // only search term was provided
-            char search_term = argv[1];
-            char input[255];
-            printf("Enter a line of text:\n");
-            char text = gets(input);
 
-            // 
-        }else{
-            char input[100];
-            int ch;
-            while((ch));
-        } 
+    // Check the parameters provided
+    if (argc == 1){ //no command given
+        printf("my-grep: searchterm [file ...]\n");
+        exit(1);
 
     }else{
-        print("my-grep: searchterm [file ...]\n");
-        exit(1);
+        // Get the search term
+        char *search_term = argv[1];
+        //char *text;
+
+        if(argc == 2){ // only search term was provided
+            // Give user access to console to type query
+            char input[255];
+            printf("Enter a line of text:\n");
+            char *as = fgets(input, sizeof input, stdin);
+            // Check if search term is in the text provided by user
+            char* status = strstr(as, search_term);
+            if(status==NULL){
+                exit(0);
+            }else{
+                printf(input);
+            }
+        }else{
+            FILE *fp;
+            for (int i = 2; i < argc; i = i + 1){
+                fp = fopen(argv[i], "r");
+                //c check if file exists
+                if(fp == NULL){
+                    printf("my grep: cannot open file\n");
+                    exit(1);
+                }else{
+                    char input[512];
+                    printf("--%s--\n", argv[i]);
+                    // read the file
+                    while(fgets(input, sizeof input, fp)!=NULL){
+                        //Check if search term is in the text provided by user
+                        char* status = strstr(input, search_term);
+                        if(status==NULL){
+                            continue;
+                        }else{
+                            printf(input);
+                        }
+                    }
+                    printf("\n");
+                }
+                fclose(fp);
+            }
+        }
     }
     return 0;
 }
